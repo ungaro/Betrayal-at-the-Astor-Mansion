8d1ee8e4e0e0e7d4162c262ce33ef1e163e9b622
Alp Guneysel <alp.guneysel@gmail.com>
2023-09-23 23:21:26 -0400

    delete 3d gameplay, a new gameplay is coming

--- Game/build/src/scenes/scripts-map.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.scriptsMap = void 0;
-/**
- * Defines the map of all available scripts in the project.
- */
-exports.scriptsMap = {};
-//# sourceMappingURL=scripts-map.js.map
