8d81a7798ade75e94f4b896fba4f0732d7a80b24
Alp Guneysel <alp.guneysel@gmail.com>
2023-09-23 23:22:34 -0400

    delete 3d gameplay, a new gameplay is coming2

--- Game/dist/bundle.js
+++
@@ -1,7 +0,0 @@
-/*! babylonjs-editor-generated-template 4.1.0 Sat Sep 23 2023 01:57:21 GMT-0400 (Eastern Daylight Time) */
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	
-/******/ 	
-/******/ })()
-;
